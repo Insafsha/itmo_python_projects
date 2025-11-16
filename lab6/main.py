4e7bd69eb56bbfa78e60118d8835ac6f47e92dae
Insafsha <sharipoffinsaf228@yandex.ru>
2025-11-16 18:25:44 +0300

    Update main.py

--- lab6/main.py
+++ lab6/main.py
@@ -1,19 +1,23 @@
-from paddle_boat import PaddleBoat
-from motor_boat import MotorBoat
+from cd import CD
+from dvd import DVD
 
 def main():
-    boat1 = PaddleBoat("Рыбачок", max_speed=8, capacity=3, paddles=2)
-    boat2 = MotorBoat("Шторм", max_speed=25, capacity=4, engine_power=90)
+    cd1 = CD(capacity_mb=700, rpm=480, laser_thickness_nm=780)
+    dvd1 = DVD(capacity_mb=4700, rpm=1600, laser_thickness_nm=650, layers=2)
 
-    print("--- Лодка на вёслах ---")
-    print(boat1.info(), "\n")
+    print("=== CD ===")
+    print(cd1.info())
+    print("Данных проходит за 20 секунд:", int(cd1.data_in_20_seconds()), "байт")
+    print("Среднее время оборота:", cd1.avg_rotation_time(), "сек\n")
 
-    print("--- Лодка с мотором ---")
-    print(boat2.info(), "\n")
+    print("=== DVD ===")
+    print(dvd1.info())
+    print("Данных проходит за 20 секунд:", int(dvd1.data_in_20_seconds()), "байт")
+    print("Среднее время оборота:", dvd1.avg_rotation_time(), "сек\n")
 
-    print("--- Перегрузка оператора + ---")
-    mixed = boat1 + boat2
-    print(mixed.info())
+    print("=== Сложение двух CD ===")
+    cd_sum = cd1 + CD(500, 300, 780)
+    print(cd_sum.info())
 
 if __name__ == "__main__":
     main()
